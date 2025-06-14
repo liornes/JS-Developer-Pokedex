ed6b94f579428b0945abae365182b45840ad6805
Leandro Lima <89042570+liornes@users.noreply.github.com>
2025-06-14 12:03:12 -0300

    Projeto desenvolvido durante as aulas

---
+++ assets/js/poke-api.js
@@ -0,0 +1,35 @@
+const PokeApi = {}
+
+function convertPokemonDetailToPokemon(pokeDetail) {
+    const pokemon = new Pokemon();
+    pokemon.number = pokeDetail.id;
+    pokemon.name = pokeDetail.name;
+
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name);
+    const [type] = types;
+
+    pokemon.types = types
+    pokemon.type = type;
+    
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default;
+
+    return pokemon;
+}
+
+PokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url)
+        .then((response) => response.json())
+        .then(convertPokemonDetailToPokemon);
+}
+
+PokeApi.getPokemons = (offset = 0, limit = 10) => {
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
+    return fetch(url)
+        .then((response) => response.json())
+        .then((jsonBody) => jsonBody.results)
+        .then((pokemons) => pokemons.map(PokeApi.getPokemonDetail))
+        .then((detailRequests) => Promise.all(detailRequests))
+        .then((pokemonsDetails) => pokemonsDetails)
+        .catch((error) => console.error(error))
+}
